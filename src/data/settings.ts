84ac2001cc1fd6c7df50bcd4b5675dceb1bd215e
Zak <nsconsulting.audit@gmail.com>
2023-11-04 14:18:53 +0100

    t

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://cannonball.littlesticks.dev',
   name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
+  title: 'Mission Better together | site temporaire',
   description: 'This is a simple splash page template built with Astro by Little Sticks',
 }
